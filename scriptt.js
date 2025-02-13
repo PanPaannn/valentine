591a43c210c385222e0913e987ec7bbec4c1b4b7
PanPaannn <bluetive5541@gmail.com>
2025-02-14 05:19:55 +0700

    innerWidth

--- scriptt.js
+++ scriptt.js
@@ -1,6 +1,6 @@
 const btnNo = document.getElementById('btnNo');
 btnNo.addEventListener('mouseover', function(){
-    let x = Math.floor(Math.random()* 1360) - 768;
+    let x = Math.floor(Math.random()* window.innerWidth) - window.innerWidth / 2;
     let y = Math.floor(Math.random()* 500) - 250 ;
 
     btnNo,this.style.left=x+"px"
