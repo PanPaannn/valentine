190b0b22da26dfef65020bd25a2d2a1f8f50bc3f
PanPaannn <bluetive5541@gmail.com>
2025-02-14 07:36:37 +0700

    innerHeight

--- scriptt.js
+++ scriptt.js
@@ -1,7 +1,7 @@
 const btnNo = document.getElementById('btnNo');
 btnNo.addEventListener('mouseover', function(){
-    let x = Math.floor(Math.random()* window.innerWidth) - window.innerWidth / 2 - 200;
-    let y = Math.floor(Math.random()* 500) - 250 ;
+    let x = Math.floor(Math.random()* window.innerWidth) - window.innerWidth / 2 - 400;
+    let y = Math.floor(Math.random()* 500) - 200 ;
 
     btnNo,this.style.left=x+"px"
     btnNo,this.style.top=y+"px"
